74038d6c2e0409a690d79f8d7b7e20567db63c1a
KayJayx <73000710+KayJayx@users.noreply.github.com>
2024-08-12 00:16:33 -0700

    Instantiated More Models and Controllers

--- design/controllers/_controller.py
+++ design/controllers/_controller.py
@@ -2,8 +2,12 @@ from __future__ import annotations
 from views.main_window_view import MainWindowView
 from views.plot_window_view import PlotWindowView
 from views.plot_controls_view import PlotControlsView
+from models.plot_window_model import PlotWindowModel
+from models.plot_controls_model import PlotControlsModel
+from controllers.plot_window_controller import PlotWindowController
+from controllers.plot_controls_controller import PlotControlsController
 
-class Controller():
+class MainController():
 
     """
     The responsibility of the Controller is to bridge the gap between the Model and View classes.
@@ -20,12 +24,15 @@ class Controller():
         self.plot_controls_view = PlotControlsView(self.main_window_view)
 
         # Create models here
+        self.plot_window_model   = PlotWindowModel()
+        self.plot_controls_model = PlotControlsModel()
 
         # Create controllers here
+        self.plot_window_controller   = PlotWindowController(self.plot_window_view, self.plot_window_model)
+        self.plot_controls_controller = PlotControlsController(self.plot_controls_view, self.plot_controls_model)
 
-        # Run the designer to actually display the info to the screen
-        #self.view.designer.Run(self.UpdateWaveform)
-        pass
+        # Run the main event handler to also render the GUI elements
+        self.main_window_view.Run(self.UpdateWaveform)
 
     def UpdateWaveform(self) -> None:
         """
